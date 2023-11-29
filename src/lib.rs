ac75c2943c153c197edd1a5ce1f7a9b617d1fba3
BazzaCipher <33623276+BazzaCipher@users.noreply.github.com>
2023-11-29 22:39:35 +1100

    I did something

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 use std::f32::consts::PI;
 use std::time::Duration;
+use std::ops::{Index, IndexMut};
 
 use nalgebra::*;
-// use octree::{point::Point3D, Octree}; // Stand-in for https://github.com/csiro-robotics/ohm
 use bvh::{
     aabb::{AABB,Bounded},
     bounding_hierarchy::BHShape,
@@ -11,16 +11,13 @@ use bvh::{
     Vector3 as Vec3,
 };
 use bno055::Error as BnoError;
+use ordered_float::NotNan;
 
 // UOM here please
-const RAY_MAXDIST: f64 = 5.0;
-const DEFAULT_HEIGHT: f64 = 10.0;
-
-// No generics ATM (but add later)
-// Consider different library without floating point points
-fn main() {
-    println!("Hello, world!");
-}
+const RAY_MAXDIST: f32 = 5.0;
+const DEFAULT_HEIGHT: f32 = 10.0;
+const HANG: f32 = 35.0;
+const VANG: f32 = 20.0;
 
 // TODO: Generic & Borrowed
 trait Pose {
@@ -30,25 +27,29 @@ trait Pose {
 
 // Vector3 for how we do it rn, allow expansion into 3D via trait
 /// Standard form: [x, y, theta]T. In metres and radians
-type RobotPose = Vector3<f32>;
+type RobotPose = Point3<f32>;
 
 impl Pose for RobotPose {
     fn pos(&self) -> Point3<f32> {
-        Point3::new(self.0, self.1, DEFAULT_HEIGHT)
+        Point3::new(self.x, self.y, DEFAULT_HEIGHT)
     }
-    fn dir(&self) -> Rotation3<f32> { Rotation3::from_euler_angles(0, 0, self.2) }
+    fn dir(&self) -> Rotation3<f32> { 
+        Rotation3::from_euler_angles(0., 0., self.z) 
+    }
 }
 
 /// Abstracts the grid-map. Is a 3D representation with the floor being the default height
 /// Allows updating for integration of data.
+// TODO: Generic over cell type; cell trait?
 trait GridMap {
     fn ray_probe(&self, ray: (&Point3<f32>, &Vector3<f32>)) -> f32;
-    fn insert(&mut self, x: &impl Pose);
+    fn insert(&mut self, x: &Point3<i32>);
     fn update(&mut self, x: &impl Pose, u: &impl SampleMeasurement);
 }
 
 struct OccupancyGrid {
-    map: BVH,
+    bvh: BVH,
+
     cells: Vec<GridCell>
 }
 
@@ -56,47 +57,74 @@ impl OccupancyGrid {
     fn build(shapes: &mut [GridCell]) -> OccupancyGrid {
         OccupancyGrid {
             cells: shapes.to_vec(),
-            map: BVH::build(shapes)
+            bvh: BVH::build(shapes)
         }
     }
 
+    /// Must rebuild the bvh in order to use ray_probe. Not my job
     fn rebuild(&mut self) {
-        self.map = BVH::build(&mut self.cells[..])
+        self.bvh = BVH::build(&mut self.cells[..]);
+    }
+}
+
+// COnsider having both
+impl<'point> Index<&'point (f32, f32, f32)> for OccupancyGrid {
+    type Output = &'point GridCell;
+
+    fn index(&self, index: &(f32, f32, f32)) -> Self::Output {
+        // Fucking shoot me; suggest octree but that'll take time
+        self.cells
+            .iter()
+            .find(|x| index == x.position)
+            .unwrap_or_default()
+    }
+}
+
+impl<'point> IndexMut<&'point (f32, f32, f32)> for OccupancyGrid {
+    fn index_mut(&mut self, index: &(f32, f32, f32)) -> &mut Self::Output {
+        self.cells
+            .iter()
+            .find(|x| index == x.position.into())
+            .unwrap_or_default()
     }
 }
 
 // Do not assume 1:1 scale
 impl GridMap for OccupancyGrid {
-    fn ray_probe(&self, ray: (&Point3<u32>, &Vector3<u32>)) -> Option<f32> {
-        // Should model behaviour of sensor (max dist etc)
-            let shapes = self.map.traverse_iterator(Ray::new(vector3tovec3(ray.0), vector3tovec3(ray.1)), &self.cells);
-            shapes
-                .map(|shape| distance(shape.position.into(), ray.0) as i64) // TODO: Compare f64 dists instead
-                .min()
+    fn ray_probe(&self, ray: (&Point3<f32>, &Vector3<f32>)) -> f32 {
+     // Should model behaviour of sensor (max dist etc)
+         self.bvh
+             .traverse_iterator(Ray::new(vector3tovec3(ray.0), vector3tovec3(ray.1)), &self.cells)
+             .map(|shape| distance(shape.position.into(), ray.0) as i16) // TODO: Compare f64 dists instead
+             .min()
+             .map(f32::from)
+             .unwrap_or(RAY_MAXDIST)
     }
     // Inefficient in that every time the map is updated, the BVH is rebuilt. Consider Fast
     // insertion algorithms
-    fn insert(&mut self, x: &RobotPose) {
-        // TODO: Handle none 1:1 case
-        self.cells
-            .push(GridCell {
-            position: *x,
-            significance: 0.3, // Reasonable initial probability
-            node_index: None,
-        })
+    // TODO: Generic squares
+    fn insert(&mut self, x: &Point3<i32>) {
+     // TODO: Handle none 1:1 case
+         self.cells
+             .push(GridCell {
+                 position: *x,
+                 intensity: NotNan::new(0.3), // Reasonable initial probability
+                 node_index: None,
+             })
     }
+
     // Recall assumption of known position for particle filters
     fn update<const VFOV: usize, const HFOV: usize>(&mut self, x: &RobotPose, u: &LaserImage<VFOV, HFOV>) {
-        // TODO: Handle none 1:1 case
-        u.integrate_into_map(x, u);
+     // TODO: Handle none 1:1 case
+     u.integrate_into_map(x, u);
     }
 }
 
 // Assume every grid cell is its bottom left coord
-#[derive(Debug, Clone)]
+#[derive(Default, Debug, Clone, Eq, PartialEq, Hash)]
 struct GridCell {
-    position: Vector3<u32>,
-    significance: f32, // Log-odds representation
+    position: Point3<i32>,
+    intensity: NotNan<f32>, // Log-odds representation
     node_index: Option<usize>,
 }
 
@@ -169,7 +197,7 @@ trait SampleControl {
 struct Imu {
     vel: Vector3<f32>,
     pos: Vector3<f32>,
-    dt: Duration,
+    dt: f32, // In seconds
 }
 
 // TODO: Eventually implement trait directly on Bno055??
@@ -177,8 +205,8 @@ impl Imu {
     fn update(&mut self, t: f32) -> Result<(), BnoError<u8>> {
         // This model suffers from drift heavily; either KF or 120+Hz updates
         // TODO: Implement Kalman for integration of accelerations (VERY IMPORTANT)
-        let ud = t * self.lin_accel()?.pos();
-        let wd = t * self.ang_accel()?.z();
+        let ud = t * self.lin_accel()?;
+        let wd = t * self.ang_accel()?.z;
         self.vel += Vector3::new(
             ud.x,
             ud.y,
@@ -197,7 +225,7 @@ impl Imu {
 impl SampleControl for Imu {
     fn motion_simulation(&self, x: &RobotPose, m: &OccupancyGrid) -> RobotPose {
         // Consider splitting into Jacobians Fp and Frl
-        x + self.dt * self.vel
+        self.dt * self.vel + *x
         // TODO: Add noise for sampling...
     }
 }
@@ -218,9 +246,13 @@ trait SampleMeasurement {
 // no overhead on each image, and we can always access the metadata. Likely
 // generics later.
 struct LaserImage<const VFOV: usize, const HFOV: usize> {
-    hang: f32, // Fixed initial viewing angle
-    vang: f32,
     inner: SMatrix<f32, VFOV, HFOV>
+}
+
+impl<const VFOV: usize, const HFOV: usize> LaserImage<VFOV, HFOV> {
+    fn new(input: SMatrix<f32, VFOV, HFOV>) -> LaserImage<VFOV, HFOV> {
+        LaserImage { inner: input }
+    }
 }
 
 /// Generic over the implementation type of the measurement for future changes
@@ -233,15 +265,15 @@ impl<const VFOV: usize, const HFOV: usize> SampleMeasurement for LaserImage<VFOV
         let left: Vector3<f32>   = lookat.cross(&up); // Will be unit vector
 
         // TODO: Use inverse of m and n for better precision
-        let m = self.hang / HFOV as f32;
-        let n = self.vang / VFOV as f32;
+        let p = HANG / HFOV as f32;
+        let q = VANG / VFOV as f32;
 
-        let mut outmatrix: SMatrix<f32, VFOV, HFOV> = Matrix::zeros();
+        let mut outmatrix: SMatrix<f32, VFOV, HFOV> = SMatrix::zeros();
 
         for i in 0..HFOV {
             for j in 0..VFOV {
-                let a = ((i - HFOV) as f32 * m).tan(); // Might be incorrect
-                let b = ((j - VFOV) as f32 * n).tan();
+                let a = ((i - HFOV) as f32 * p).tan(); // Might be incorrect
+                let b = ((j - VFOV) as f32 * q).tan();
 
                 let ray = lookat + (a * left + b * up - lookat).normalize();
 
@@ -249,18 +281,18 @@ impl<const VFOV: usize, const HFOV: usize> SampleMeasurement for LaserImage<VFOV
                 outmatrix[(i, j)] = m.ray_probe(
                     (
                         x,
-                        ray
+                        &ray
                     )
                 );
             }
         }
         
-        outmatrix
+        LaserImage { inner: outmatrix }
     }
     fn integrate_into_map(&self, x: &RobotPose, m: &mut OccupancyGrid) {
         // Important behaviour: Point3.to_homogeneous is different to Vector3
         // 1. Invert transformation of camera coords in terms of world frame
-        let m = Isometry3::<f32>::new(
+        let msb = Isometry3::<f32>::new(
             Vector3::new(x.x, x.y, 0.), // Zero by assumption of robot pose
             Vector3::new(0., 0., x.z),
         ).inverse();
@@ -269,17 +301,20 @@ impl<const VFOV: usize, const HFOV: usize> SampleMeasurement for LaserImage<VFOV
         let lookat: Vector3<f32> = x.dir().transform_vector(&Vector3::x_axis());
         let left: Vector3<f32>   = lookat.cross(&up); // Will be unit vector
 
-        let m = self.hang / HFOV as f32;
-        let n = self.vang / VFOV as f32;
+        let p = HANG / HFOV as f32;
+        let q = VANG / VFOV as f32;
 
         for j in 0..VFOV {
             for i in 0..HFOV {
                 let cnt = j * VFOV + i;
 
-                let a = ((i - HFOV) as f32 * m).tan(); // Might be incorrect
-                let b = ((j - VFOV) as f32 * n).tan();
+                let a = ((i - HFOV) as f32 * p).tan(); // Might be incorrect
+                let b = ((j - VFOV) as f32 * q).tan();
 
-                let ray = lookat + (a * left + b * up - lookat).new_normalize();
+                let ray = lookat + (a * left + b * up - lookat).normalize();
+                let pt = Point3::new(ray.x, ray.y, ray.z); // TODO: Idiomatic conversion
+
+                m[msb.transform_point(&pt)].intensity += 0.1;
             }
         }
     }
